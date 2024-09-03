686c8e6a4c4be97f54985616cc12f15b116f03c8
Arpit Aggarwal <117593103+arpitaggarwal0511@users.noreply.github.com>
2024-09-03 15:35:20 +0530

    initial

---
+++ app.js
@@ -0,0 +1,7 @@
+import react from "react";
+import reactDom from "react-dom/client";
+const root = reactDom.createRoot(document.getElementById("root"));
+
+const heading = <h1 id="oka">ayo nigg</h1>;
+
+root.render(heading);
