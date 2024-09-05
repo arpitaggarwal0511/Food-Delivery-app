8631b1d2ca2cdb5a8a665372747d5d1f2871ef96
Arpit Aggarwal <117593103+arpitaggarwal0511@users.noreply.github.com>
2024-09-05 10:44:54 +0530

    feat:filter-btn

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,30 @@
 import Rescard from "./Rescard";
 import resList from "../utils/mockData";
-const Body = () => {
-    return (
-      <div className="body">
-        <div className="search">search bar here !</div>
-        <div className="res-cont">
-      {
-          resList.map((rest)=>(
-              <Rescard key={rest.info.id } resData={rest}/>
-          ))
-      }
-        </div>
-      </div>
-    );
-  };
+import {useState} from "react";
 
-  export default Body;
+const Body = () => {
+  const [ulist,setulist ]=useState(resList);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const flist = ulist.filter((res) => res.info.avgRating > 4.5);
+            setulist(flist);
+          }}  
+        >
+          Top Rated Restaurants !
+        </button>
+      </div>
+
+      <div className="res-cont">
+        {resList.map((rest) => (
+          <Rescard key={rest.info.id} resData={rest} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
