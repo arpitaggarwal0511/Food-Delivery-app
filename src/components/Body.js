d9d450ea0d7db80d3ae29a8869b56a6539ff5f40
Arpit Aggarwal <117593103+arpitaggarwal0511@users.noreply.github.com>
2024-09-08 00:25:11 +0530

    feat:search bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,37 +1,54 @@
 import Rescard from "./Rescard";
 import Shimmer from "./Shimmer";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
-  const[filterRest,setFilterRest]=useState([]);
+  const [filterRest, setFilterRest] = useState([]);
 
-  const[searchtext,setsearchtext]=useState("");
+  const [searchtext, setsearchtext] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchdata();
-  },[]);
- const fetchdata=async ()=>{
-   const data = await fetch(
-    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-  );
-    const json= await data.json();
-setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-setFilterRest(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }, []);
+  const fetchdata = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.7333148&lng=76.7794179&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilterRest(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
-  }
- 
-  return listOfRestaurants.length==0 ? <Shimmer/> :(
+  return listOfRestaurants.length == 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
-      <div className="search">
-        <input type="text" value={searchtext} onChange={(e)=>{setsearchtext(e.target.value)}} className="searchbox"/>
-        <button onClick={()=>{
-          fetchdata();
-          const filterRest = listOfRestaurants.filter((res)=>res.info.name.toLowerCase().includes(searchtext.toLowerCase()));
-          setFilterRest(filterRest);
+        <div className="search">
+          <input
+            type="text"
+            value={searchtext}
+            onChange={(e) => {
+              setsearchtext(e.target.value);
+            }}
+            className="searchbox"
+          />
 
-        }}>Search</button>
+          <button
+            onClick={() => {
+              const filterRest = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchtext.toLowerCase())
+              );
+              setFilterRest(filterRest);
+            }}
+          >
+            Search
+          </button>
         </div>
         <button
           className="filter-btn"
@@ -53,7 +70,6 @@ setFilterRest(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.res
       </div>
     </div>
   );
-
 };
 
 export default Body;
